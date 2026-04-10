c0dad93af81a1d5ca5ffe74a7b5e18a1962c383a
Juan Vásquez <juan@ombulabs.com>
2026-04-09 23:31:39 -0600

    Simplify MaquinaComponentsHelper to just include engine modules
    Remove unnecessary wrapper methods. The helper only needs to
    include the engine modules so their methods are available in views.

--- app/helpers/maquina_components_helper.rb
+++ app/helpers/maquina_components_helper.rb
@@ -4,20 +4,4 @@ module MaquinaComponentsHelper
   include MaquinaComponents::SidebarHelper
   include MaquinaComponents::IconsHelper
   include MaquinaComponents::ToastHelper
-
-  def main_icon_svg_for(name)
-    nil
-  end
-
-  def app_sidebar_state(cookie_name = "sidebar_state")
-    sidebar_state(cookie_name)
-  end
-
-  def app_sidebar_open?(cookie_name = "sidebar_state")
-    sidebar_open?(cookie_name)
-  end
-
-  def app_sidebar_closed?(cookie_name = "sidebar_state")
-    sidebar_closed?(cookie_name)
-  end
 end
